7515f625cc6a6dbd94054cdc9012e025ee0f53d9
Ívan Bjarni Jónsson <ibj9@hi.is>
2014-10-28 16:49:49 +0000

    A prototype for the EntityManager has been added. The current entity manager does not support high velocity transformers, then again it doesn't support any transformers at all, that feature is still in progress

---
+++ EntityManager.js
@@ -0,0 +1,84 @@
+//=====================================================
+// Class: EntityManager
+// Date : 28.October 2014
+// Programmed by: FutureDevs
+//=====================================================
+
+
+/*----------------------
+		Constructor
+------------------------*/
+function EntityManager(descr) {
+    for (var property in descr) {
+        this[property] = descr[property];
+    }
+}
+
+/*----------------------
+	Instance Variables
+------------------------*/
+EntityManager.prototype.player = null;
+EntityManager.prototype.bullets = [];
+EntityManager.prototype.enemies = [];
+EntityManager.prototype.boss = null;
+
+
+/*----------------------
+	Add and initialize
+------------------------*/
+EntityManager.prototype.setPlayer = function(player)
+{this.player=player;}
+
+EntityManager.prototype.setBoss = function(boss)
+{this.boss=boss;}
+
+EntityManager.prototype.addBullet = function(bullet)
+{
+	var low = getLowestAvailable(this.bullets);
+	this.bullets[low]=bullet;
+}
+
+EntityManager.prototype.addEnemy = function(enemy)
+{
+	var low = getLowestAvailable(this.enemies);
+	this.enemy[low]=enemy;
+}
+
+
+/*----------------------
+		Update
+------------------------*/
+EntityManager.prototype.updateTime = function(du)
+{
+	timer += du;
+}
+
+EntityManager.prototype.update = function(du)
+{
+	if(this.player)
+		this.player.update(du);
+	for(var bullet in this.bullets)
+		bullet.update(du);
+	for (var enemy in this.enemies) 
+		enemy.update(du);
+	if(this.boss)
+		this.boss.update();
+
+	this.updateTime(du)
+}
+
+
+/*----------------------
+		Render
+------------------------*/
+EntityManager.prototype.render = function(du)
+{
+	if(this.player)
+		this.player.render(du);
+	for(var bullet in this.bullets)
+		bullet.render(du);
+	for (var enemy in this.enemies) 
+		enemy.render(du);
+	if(this.boss)
+		this.boss.render();
+}
