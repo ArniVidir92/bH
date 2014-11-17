58ccb09edbc062a810dc02d20fda9f0be6f79526
Vilhelm Friðriksson <vfridriks@simnet.is>
2014-11-17 13:05:59 +0000

    Level one?

--- LevelManager.js
+++ LevelManager.js
@@ -16,23 +16,35 @@ function LevelManager(descr) {
     this.init();
 }
 
+
 /*----------------------
 	Instance Variables
 ------------------------*/
 LevelManager.prototype.timer = 1;
 LevelManager.prototype.atimers = [];
+LevelManager.prototype.mainBoss = new Boss({
+    										cx : 250,
+    										cy : 100,
+											});
 
 LevelManager.prototype.blackKnights = {
     array 		: [],
     spawnNumber : 10,
-    spawnTime   : 3,
+    spawnTime   : 5,
     spawnTimer  : 0, 
 };
 
 LevelManager.prototype.grayKnights = {
     array 		: [],
     spawnNumber : 1,
-    spawnTime   : 2,
+    spawnTime   : 3,
+    spawnTimer  : 0, 
+};
+
+LevelManager.prototype.christmasCarols = {
+    array 		: [],
+    spawnNumber : 2,
+    spawnTime   : 4,
     spawnTimer  : 0, 
 };
 
@@ -62,7 +74,7 @@ LevelManager.prototype.init = function()
 	/*-----------------------------
 		Creating Gray Knights  
 	------------------------------*/
-	for(var i = 0; i < 100; i++)
+	for(var i = 0; i < 20; i++)
 	{
     	var en = new Enemy({
     	cx 		: 	470,
@@ -76,11 +88,30 @@ LevelManager.prototype.init = function()
     	this.grayKnights.array.push(en);
 	}
 
+	/*-----------------------------
+		Creating Christmas Carols
+	------------------------------*/
+	xCord = 30;
+	for(var i = 0; i < 30; i++)
+	{
+    	var en = new Enemy({
+    	cx 		: 	xCord,
+    	cy 		: 	-5,
+    	vx 		: 	0,
+    	vy 		: 	1,
+    	type	: 	"ChristmasCarol",
+
+    	});
+ 		if(xCord === 30) xCord = g_GameCanvas - 30;
+    	else xCord = 30;
+    	this.christmasCarols.array.push(en);
+	}
 
 
 	//Putting enemy objects in array for easy timers
 	this.atimers[0] = this.blackKnights;
 	this.atimers[1] = this.grayKnights;
+	this.atimers[2] = this.christmasCarols;
 }
 
 
@@ -106,9 +137,6 @@ LevelManager.prototype.update = function(du)
 {
 	
 	this.updateTimers(du);
-
-	
-
 	if(this.grayKnights.spawnTimer > this.grayKnights.spawnTime)
 	{
 		this.grayKnights.spawnTimer = 0;
@@ -120,6 +148,12 @@ LevelManager.prototype.update = function(du)
 		this.blackKnights.spawnTimer = 0;
 		this.spawnEnemies(this.blackKnights);
 	}
+	if(this.christmasCarols.spawnTimer >= this.christmasCarols.spawnTime)
+	{	
+		this.christmasCarols.spawnTimer = 0;
+		this.spawnEnemies(this.christmasCarols);
+	}
+	if(this.timer > 60) entityManager.setBoss(this.mainBoss);
 
 }
 
